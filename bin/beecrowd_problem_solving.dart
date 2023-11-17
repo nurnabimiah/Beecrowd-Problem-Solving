202243208c5a9e675f581b9a0d99109e739e2c9b
md.nurnabimiah <diunayonahmed@gmail.com>
2023-11-17 11:35:26 +0600

    write triangle with star

--- bin/beecrowd_problem_solving.dart
+++ bin/beecrowd_problem_solving.dart
@@ -4,34 +4,20 @@ import 'package:beecrowd_problem_solving/beecrowd_problem_solving.dart' as beecr
 
 import 'dart:io';
 
+//9.Write a program in C to display a pattern like a right angle triangle using an asterisk.
+
 void main() {
 
-
-  print('Enter Take positive number: ');
-  String? input = stdin.readLineSync();
-  int? num = int.tryParse(input!);
-  int i =1;
-  int count = 0;
-
-
-  for(i = i+1; i<=num!-1;i++)
+  int i,j;
+  for(i=1;i<=5;i++)
     {
-      if(num%i == 0)
+      for(j=1;j<=i;j++)
         {
-          count ++;
-          break;
-
+          stdout.write('*');
         }
-
+        print ('');
     }
 
-  if(count==0)
-    {
-      print('Prime Number');
-    }
-  else{
-    print('Not Prime Number');
-  }
 
 
 }
