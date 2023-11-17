de4dda318c918b98394969e00bc899aa6577ad52
md.nurnabimiah <diunayonahmed@gmail.com>
2023-11-17 10:56:19 +0600

    prime number check

--- bin/beecrowd_problem_solving.dart
+++ bin/beecrowd_problem_solving.dart
@@ -6,15 +6,32 @@ import 'dart:io';
 
 void main() {
 
-  int i;
-  String? input   = stdin.readLineSync();
-  int? num = int.tryParse(input!);
 
-  for(i=1; i<=num!; i++)
+  print('Enter Take positive number: ');
+  String? input = stdin.readLineSync();
+  int? num = int.tryParse(input!);
+  int i =1;
+  int count = 0;
+
+
+  for(i = i+1; i<=num!-1;i++)
     {
-       if(num % i == 0){
-         print(i);
-       }
+      if(num%i == 0)
+        {
+          count ++;
+          break;
+
+        }
+
     }
+
+  if(count==0)
+    {
+      print('Prime Number');
+    }
+  else{
+    print('Not Prime Number');
+  }
+
 
 }
